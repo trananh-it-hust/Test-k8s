dd363febc96c9dd2bc11fae1189a193120cb3841
trananh-it-hust <haianhtranmanh@gmail.com>
2025-05-12 14:19:28 +0700

    Test12

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end(
-    `Server IP: ${serverIP}\nVersion: ${version}\nHostname: ${hostname}\nUpdated: ${new Date().toISOString()}\nHello from Hải Anh`
+    `Server IP: ${serverIP}\nVersion: ${version}\nHostname: ${hostname}\nUpdated: ${new Date().toISOString()}\n`
   );
 });
 
